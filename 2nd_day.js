0055185bf7da761107d7b4a192c19ec2090dc1ca
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-01-19 19:10:03 +0600

    2nd day first problem solved

---
+++ 2nd_day.js
@@ -0,0 +1,22 @@
+// ================ problem 5 =======================
+
+// Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result.
+
+// Define an array of objects representing people
+const people = [
+  { name: "Mazharul", age: 23, gender: "male" },
+  { name: "Marufa", age: 17, gender: "female" },
+  { name: "Faisal", age: 23, gender: "male" },
+  { name: "Sara", age: 16, gender: "female" },
+  { name: "Tabassum", age: 19, gender: "female" },
+];
+
+const filterAndMapFemales = (peopleArray) => {
+  const females = peopleArray.filter((person) => person.gender === "female");
+  const femaleNames = females.map((person) => person.name);
+  return femaleNames;
+};
+
+const femaleFilterResult = filterAndMapFemales(people);
+
+// console.log(femaleFilterResult);

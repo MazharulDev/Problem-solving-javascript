d9791fec3b340aac2244e5b16c32a3cdcba6eb2b
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-01-22 22:48:40 +0600

    2nd day 2nd problem solved

--- 2nd_day.js
+++ 2nd_day.js
@@ -19,3 +19,22 @@ const filterAndMapFemales = (peopleArray) => {
 const femaleFilterResult = filterAndMapFemales(people);
 
 // console.log(femaleFilterResult);
+
+// Task: Object Manipulation
+// Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "1984", author: "George Orwell", year: 1949 },
+  { title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+];
+
+const extractBookTitles = (bookArray) => {
+  return bookArray.map((book) => book.title);
+};
+
+const bookTitles = extractBookTitles(books);
+
+// console.log(bookTitles);

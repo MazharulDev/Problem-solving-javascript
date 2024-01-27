cf96ec7cc9cebf40603db8222d7325b06973f780
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-01-27 16:10:54 +0600

    problem 11 is solved in 3rd day

--- 3rd_day.js
+++ 3rd_day.js
@@ -1,4 +1,20 @@
 // ================ problem 11 =======================
+
+// Task: Array Reduction
+// Create an array of numbers. Write a function that uses the reduce method to calculate the sum of all even numbers in the array.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sumEvenNumbers = (numbers) => {
+  return numbers.reduce((accumulator, currentValue) => {
+    if (currentValue % 2 === 0) {
+      accumulator += currentValue;
+    }
+    return accumulator;
+  }, 0);
+};
+
+const evenSum = sumEvenNumbers(numbers);
+console.log(evenSum);
 // ================ problem 12 =======================
 // ================ problem 13 =======================
 // ================ problem 14 =======================

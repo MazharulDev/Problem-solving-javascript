6fb94acc84186dd3f7655ef923313244e26eded9
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-02-03 21:46:23 +0600

    4th day problem created

--- 4th_day.js
+++ 4th_day.js
@@ -1,1 +1,5 @@
 // ================ problem 16 =======================
+// PW2.01.01 Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.

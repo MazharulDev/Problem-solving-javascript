358d9431741e3e4ebe292a3605c56d637118ab9b
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-02-04 23:07:08 +0600

    problem 16 is solved

--- 4th_day.js
+++ 4th_day.js
@@ -3,3 +3,31 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+const twoSum = (nums, target) => {
+  const numMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+
+    numMap.set(nums[i], i);
+  }
+
+  return [];
+};
+
+const nums1 = [2, 7, 11, 15];
+const target1 = 9;
+console.log(twoSum(nums1, target1));
+
+const nums2 = [3, 2, 4];
+const target2 = 6;
+console.log(twoSum(nums2, target2));
+
+const nums3 = [3, 3];
+const target3 = 6;
+console.log(twoSum(nums3, target3));
